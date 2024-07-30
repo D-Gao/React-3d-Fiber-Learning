04edfb2744b1adc606575750097f054e76fcd3b8
D-Gao <huang245415359@gmail.com>
2024-07-30 18:06:41 +0200

    bug fix on resize

--- src/experiences/Morph/MorphExperience.tsx
+++ src/experiences/Morph/MorphExperience.tsx
@@ -17,12 +17,6 @@ type GLTFResult = GLTF & {
   };
 };
 
-const sizes = {
-  width: window.innerWidth,
-  height: window.innerHeight,
-  pixelRatio: Math.min(window.devicePixelRatio, 2),
-};
-
 extend(MorphMaterial);
 
 const MorphExperience: FC = () => {
@@ -30,6 +24,23 @@ const MorphExperience: FC = () => {
   const pointsRef = useRef<THREE.Points>(null);
   const materialRef = useRef<THREE.ShaderMaterial>(null);
   const morphIndex = useRef<number>(0);
+  const particles = useRef<{
+    maxCount: number;
+    positions: THREE.Float32BufferAttribute[];
+    geometry: THREE.BufferGeometry | null;
+    index: number;
+  }>({
+    maxCount: 0,
+    positions: [],
+    geometry: null,
+    index: 0,
+  });
+
+  const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight,
+    pixelRatio: Math.min(window.devicePixelRatio, 2),
+  };
 
   useEffect(() => {
     const pixelRatio = Math.min(window.devicePixelRatio, 2);
@@ -40,7 +51,7 @@ const MorphExperience: FC = () => {
     "/models/models.glb"
   ) as unknown as GLTFResult;
 
-  const particles: {
+  /* const particles.current: {
     maxCount: number;
     positions: THREE.Float32BufferAttribute[];
     geometry: THREE.BufferGeometry | null;
@@ -50,29 +61,39 @@ const MorphExperience: FC = () => {
     positions: [],
     geometry: null,
     index: morphIndex.current,
-  };
+  }; */
+
+  useEffect(() => {
+    window.addEventListener("resize", () => {
+      // Update sizes
+      sizes.width = window.innerWidth;
+      sizes.height = window.innerHeight;
+      sizes.pixelRatio = Math.min(window.devicePixelRatio, 2);
+
+      console.log(particles.current);
+    });
+  }, []);
 
   useEffect(() => {
     const positions = scene.children.map((child) => {
       if (child instanceof THREE.Mesh)
         return (child as THREE.Mesh).geometry.attributes.position;
     });
-    console.log(positions);
 
-    // need to get the max count of particles for among all children mesh
-    particles.maxCount = 0;
+    // need to get the max count of particles.current for among all children mesh
+    particles.current.maxCount = 0;
     for (const position of positions) {
-      if (position!.count > particles.maxCount) {
-        particles.maxCount = position!.count;
+      if (position!.count > particles.current.maxCount) {
+        particles.current.maxCount = position!.count;
       }
     }
 
     //
     for (const position of positions) {
       const originalArray = position!.array; //in this position array each 3 number corrisponds to x,y,z coordinates of each vertices
-      const newArray = new Float32Array(particles.maxCount * 3); // For 10k vertices, the array must be 3 times bigger for x y z coordinates. Also, it is necessary to create a new float 32 array because it's length can't be changed once it's been created so we create a new one here
+      const newArray = new Float32Array(particles.current.maxCount * 3); // For 10k vertices, the array must be 3 times bigger for x y z coordinates. Also, it is necessary to create a new float 32 array because it's length can't be changed once it's been created so we create a new one here
 
-      for (let i = 0; i < particles.maxCount; i++) {
+      for (let i = 0; i < particles.current.maxCount; i++) {
         const i3 = i * 3;
 
         if (i3 < originalArray.length) {
@@ -87,25 +108,27 @@ const MorphExperience: FC = () => {
           newArray[i3 + 2] = originalArray[randomIndex + 2];
         }
       }
-      particles.positions.push(new THREE.Float32BufferAttribute(newArray, 3)); // the GPU needs to know that it has to take values 3 by 3
+      particles.current.positions.push(
+        new THREE.Float32BufferAttribute(newArray, 3)
+      ); // the GPU needs to know that it has to take values 3 by 3
     }
 
     // Geometry
-    const sizesArray = new Float32Array(particles.maxCount);
+    const sizesArray = new Float32Array(particles.current.maxCount);
 
-    for (let i = 0; i < particles.maxCount; i++) {
+    for (let i = 0; i < particles.current.maxCount; i++) {
       sizesArray[i] = Math.random();
     }
 
     pointsRef.current!.geometry = new THREE.BufferGeometry();
     pointsRef.current!.geometry.setAttribute(
       "position",
-      particles.positions[particles.index]
+      particles.current.positions[particles.current.index]
     );
     pointsRef.current!.geometry.setAttribute(
       "aPositionTarget",
-      particles.positions[3]
-    ); // when importing other models from Blender, check if particles positions array has enough elements to choose from or it might crash
+      particles.current.positions[3]
+    ); // when importing other models from Blender, check if particles.current positions array has enough elements to choose from or it might crash
     pointsRef.current!.geometry.setAttribute(
       "aSize",
       new THREE.BufferAttribute(sizesArray, 1)
@@ -122,15 +145,27 @@ const MorphExperience: FC = () => {
     console.log("clicked");
 
     pointsRef.current!.geometry.attributes.position =
-      particles.positions[morphIndex.current];
+      particles.current.positions[morphIndex.current];
     pointsRef.current!.geometry.attributes.aPositionTarget =
-      particles.positions[nextIndex];
+      particles.current.positions[nextIndex];
 
     // Animate uProgress and update frame on demand
     await gsap.fromTo(
       materialRef.current!.uniforms.uProgress,
       { value: 0 },
-      { value: 1, duration: 3, ease: "linear", onUpdate: invalidate } // linear because transition's easing is already handled in vertex.glsl
+      {
+        value: 1,
+        duration: 3,
+        ease: "linear",
+        onUpdate: function () {
+          console.log("Invalidating frame");
+          invalidate();
+          /* setFrameloop("always"); */
+        },
+        /* onComplete: function () {
+          setFrameloop("demand");
+        }, */
+      } // linear because transition's easing is already handled in vertex.glsl
     );
     // Save index
     morphIndex.current = nextIndex;
